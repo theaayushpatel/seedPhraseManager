e9b033db4a0ebcc10c800f902112a45c0608385a
theaayushpatel <aayushp9326@gmail.com>
2025-06-01 08:55:18 +0530

    modified MongoDB link

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // MongoDB connection
-mongoose.connect('mongodb://localhost:27017/seedPhraseManager')
+mongoose.connect('mongodb://127.0.0.1:27017/seedPhraseManager')
     .then(() => console.log('Connected to MongoDB'))
     .catch((error) => console.error('Error connecting to MongoDB:', error));
 
